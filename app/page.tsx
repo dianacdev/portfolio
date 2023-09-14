45065eb42165c91780ec7806e01cdc8fc235014f
Diana C <diana@gmetrix.com>
2023-09-14 15:29:56 -0600

    Changed Header to Image Background

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ export default function Home() {
   return (
     <div>
      <div className='mx-[10%] mt-[8%]'>
+
         <div className='relative w-full h-screen mx-auto'>
-          <div className='absolute w-[20%] h-5 bg-green-500/30 right-5 animate-pulse'/>
+          {/* <div className='absolute w-[20%] h-5 bg-green-500/30 right-5 animate-pulse'/>
           <div className='absolute w-[40%] h-5 bg-green-500/30 right-[100px] top-[150px] animate-pulse'/>
           <div className='absolute w-[40%] h-5 bg-emerald-700/30 right-10 top-[280px] animate-pulse'/>
           <div className='absolute w-[60%] h-5 bg-lime-500/30 right-[100px] top-[250px] animate-pulse'/>
@@ -21,12 +22,14 @@ export default function Home() {
           <div className='absolute w-[40%] h-5 bg-green-500/1 0 left-[100px] top-[220px] animate-pulse'/>
           <div className='absolute w-[40%] h-5 bg-emerald-700/30 left-10 top-[450px] animate-pulse'/>
           <div className='absolute w-[60%] h-5 bg-lime-500/30 left-[100px] top-[350px] animate-pulse'/>
-          <div className='absolute w-[80%] h-5 bg-emerald-600/30 left-20 top-[320px] animate-pulse'/>
+          <div className='absolute w-[80%] h-5 bg-emerald-600/30 left-20 top-[320px] animate-pulse'/> */}
 
-          <h1 className='text-6xl'>Hello, I&apos;m <span className='text-green-500'>Diana.</span></h1>
-          <sub className='text-lg font-light text-slate-400 mt-2'>
-            Creating User Interfaces, Web Applications and more since 2017.
-          </sub>
+            <div className='bg-header-pattern bg-cover bg-no-repeat bg-center'>
+            <h1 className='text-6xl'>Hello, I&apos;m <span className='text-green-500'>Diana.</span></h1>
+            <sub className='text-lg font-light text-slate-400 mt-2'>
+              Creating User Interfaces, Web Applications and more since 2017.
+            </sub>
+            </div>
         </div>
       </div>
       <div className='z-0 relative p-4'>
